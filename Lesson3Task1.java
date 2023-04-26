e1e2bf63b2c7267a7be940344978bf4a53d57461
Кирилл <120108672+KirillCH1@users.noreply.github.com>
2023-04-26 20:47:58 +0300

    Update Lesson3Task1.java

--- Lesson3Task1.java
+++ Lesson3Task1.java
@@ -12,18 +12,14 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<Integer>();
         enterNumbers(numbers);
-            // Создаем итератор для списка чисел
             Iterator<Integer> iterator = numbers.iterator();
             while (iterator.hasNext()) {
                 Integer number = iterator.next();
-                // Проверяем, является ли число четным
                 if (number % 2 == 0) {
-                    // Удаляем четное число из списка
                     iterator.remove();
                 }
             }
 
-            // Выводим все нечетные числа
             for (Integer number : numbers) {
                 System.out.println(number);
             }
