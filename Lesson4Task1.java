68728f35b6f6e63bf46f1483ee6a7b7376c11eaa
Кирилл <120108672+KirillCH1@users.noreply.github.com>
2023-04-28 20:42:01 +0300

    Update Lesson4Task1.java

--- Lesson4Task1.java
+++ Lesson4Task1.java
@@ -1,23 +1,27 @@
 
+
 /*
     Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список. Постараться не обращаться к листу по индексам.
 */
 
-
-import java.util.Arrays;
 import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args){
-        LinkedList list = new LinkedList(Arrays.asList(1, 2, 3, 4, 5));
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
         LinkedList revers =Linked(list);
         System.out.println(revers);
         }
     public static LinkedList Linked (LinkedList list) {
-        LinkedList reversedList = new LinkedList();
+        LinkedList revers = new LinkedList();
         while (!list.isEmpty()) {
-            reversedList.addFirst(list.removeFirst());
+            revers.addFirst(list.removeFirst());
         }
-        return reversedList;
+        return revers;
     }
 }
